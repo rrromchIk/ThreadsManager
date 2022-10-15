0f73dc8982314e9fe44c411b70db4a0ff11fe2ac
rrromchIk <90086332+rrromchIk@users.noreply.github.com>
2022-10-15 15:56:37 +0300

    Update threadfactory.h

--- threadfactory.h
+++ threadfactory.h
@@ -6,27 +6,44 @@
 
 class ThreadFactory {
 private:
+    //for all threads
     int numOfThreads;
     HANDLE *threads;
     DWORD *threadsID;
+
+    //for updating tanble
     HANDLE tableManager;
+
+    //MUTEX
     HANDLE mutex;
 
+    //SEMAPHORE
+    HANDLE semaphore;
+
+    //BUSY WAITING
+    static long isBusy;
+
+    //common resource
     static long sumOfElements;
 
+    //array for calculation sum
     int* array;
     int arraySize;
     void initialiseArray();
     void calculateSumInCertainInterval(int start, int end);
 
+    //deviding array into intervals
     int begin;
     int end;
+
 public:
     ThreadFactory(int numOfThreads, int arraySize);
     void setPriority(int priority);
     void resumeThread();
     void suspendThread();
     void terminateThread();
+    void terminateAllThreads();
+
     int getSum() const;
     std::string arrayToString() const;
     ~ThreadFactory();
